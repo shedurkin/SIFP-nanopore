d44aaeefbe349f3365765d83f4791d134ef26d70
Kathleen Durkin <kdurkin1@n3439.hyak.local>
2025-10-29 14:59:58 -0700

    script for dorado w GPU

---
+++ D-Group4/code/03.01_G4L1_MinION_Dorado.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=G4L1_MinION_Dorado
+## Allocation Definition
+#SBATCH --account=srlab-ckpt
+#SBATCH --partition=ckpt
+## Resources
+## GPU
+#SBATCH --gres=gpu:p100:1
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=0-02:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kdurkin1@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/srlab/kdurkin1/SIFP-nanopore/D-Group4/output/03.01-G4-Library1-MinION-Dorado-recall-GPU/
+
+## Script for running ONT Dorado to perform
+## basecalling (i.e. convert raw ONT pod5 to FastQ) of NanaPore data generated
+## Summer 2025, as psrt of K.Durkin SIFP project
+
+## This script utilizes a GPU node. These nodes are only available as part of the checkpoint
+## partition/account. Since we don't own a GPU node, our GPU jobs are lowest priority and
+## can be interrupted at any time if the node owner submits a new job.
+
+###################################################################################
+# These variables need to be set by user
+
+wd=$(pwd)
+
+# Programs array
+declare -A programs_array
+programs_array=(
+[dorado]="apptainer exec /gscratch/srlab/containers/srlab-R4.4-bioinformatics-container-3886a1c.sif dorado"
+)
+
+# Establish variables for more readable code
+
+# Input files directory
+raw_pod5_dir=/gscratch/srlab/kdurkin1/SIFP-nanopore/D-Group4/data/03.01-G4-Library1-MinION-Dorado-recall-GPU/pod5_pass
+output_dir=/gscratch/srlab/kdurkin1/SIFP-nanopore/D-Group4/output/03.01-G4-Library1-MinION-Dorado-recall-GPU/
+genome_file=/gscratch/srlab/kdurkin1/SIFP-nanopore/data/GCA_965233905.1_jaEunKnig1.1/GCA_965233905.1_jaEunKnig1.1_genomic.fna
+
+# Output directory
+out_dir=${wd}
+
+# CPU threads
+threads=28
+
+# Sequencing kit used
+kit="SQK-NBD114-96"
+
+# Flow Cell ID
+flow_cell_id="FBD08455"
+
+# GPU devices setting
+GPU_devices=auto
+
+# Set number of FastQ sequences written per file (0 means all in one file)
+records_per_fastq=0
+
+###################################################################################
+
+# Exit script if any command fails
+set -e
+
+# Load Python Mox module for Python module availability
+module load intel-python3_2017
+
+# Load CUDA GPU module
+module load cuda/10.1.105_418.39
+
+$dorado basecaller \
+hac \
+-r ${raw_pod5_dir}/ \
+--kit-name ${kit} \
+--trim 'all' \
+--reference ${genome_file} \
+--modified-bases 5mCG_5hmCG 6mA \
+--device ${GPU_devices} \
+> ${output_dir}/${flow_cell_id}_pass_recalled.bam
+
+
+###################################################################################
+
+# Document programs in PATH (primarily for program version ID)
+{
+date
+echo ""
+echo "System PATH for $SLURM_JOB_ID"
+echo ""
+printf "%0.s-" {1..10}
+echo "${PATH}" | tr : n
+} >> system_path.log
+
+
+# Capture program options
+for program in "${!programs_array[@]}"
+do
+	{
+  echo "Program options for ${program}: "
+	echo ""
+	${programs_array[$program]} --help
+	echo ""
+	echo ""
+	echo "----------------------------------------------"
+	echo ""
+	echo ""
+} &>> program_options.log || true
+done
